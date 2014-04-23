ffd1d3e12dfff9e96b57520e395a97a586877337
zjuwwq <zjuwwq@163.com>
2014-04-23 17:59:55 +0800

    placeholder

---
+++ src/placeholder.js
@@ -0,0 +1,45 @@
+angular.module('zjuwwq.placeholder', [])
+	.constant('placeholderConfig', {
+		className: 'u-placeholder'
+	})
+	.directive('placeholder', ['placeholderConfig',
+		function(placeholderConfig) {
+			// if browser support
+			var isSupport = 'placeholder' in document.createElement('input');
+			if (isSupport) return {};
+
+			var className = placeholderConfig.className;
+			return {
+				restrict: 'A',
+				require: '?ngModel',
+				link: function(scope, element, attrs, ngModel) {
+					var placeholder = attrs.placeholder;
+					if (!placeholder || attrs.type === 'password') return;
+
+					function addPlaceholder() {
+						if (!element.val()) {
+							element.val(placeholder);
+							element.addClass(className);
+						}
+					}
+					element.on('focus', function() {
+						if (element.val() === placeholder) {
+							element.val('');
+							element.removeClass(className);
+						}
+					}).on('blur', addPlaceholder);
+					addPlaceholder();
+
+					// use with ng-model
+					if (ngModel) {
+						ngModel.$parsers.unshift(function(value) {
+							return value === placeholder ? '' : value;
+						});
+						ngModel.$formatters.push(function(value) {
+							return value == undefined || value === '' ? placeholder : '';
+						});
+					}
+				}
+			};
+		}
+	]);
---
+++ test/placeholder.spec.js
@@ -0,0 +1,50 @@
+describe('placeholder directive', function(){
+  var $compile, scope, element, className;
+
+  beforeEach(module('zjuwwq.placeholder'));
+
+  beforeEach(inject(function(_$compile_, _$rootScope_, _placeholderConfig_){
+    $compile = _$compile_;
+    scope = _$rootScope_.$new();
+    className = _placeholderConfig_.className;
+  }));
+
+  function compile(){
+    element = $compile('<input ng-model="name" placeholder="用户名">')(scope).appendTo($('body'));
+  }
+
+  it('add placeholder on initialization', function(){
+    compile();
+    expect(element.val()).toEqual(element.attr('placeholder'));
+    expect(element.hasClass(className)).toBeTruthy();
+    scope.$digest();
+    expect(scope.name).toBe('');
+  });
+
+
+  it('remove placeholder on focus empty input', function(){
+    compile();
+    element.focus();
+    expect(element.val()).toBe('');
+    expect(element.hasClass(className)).toBeFalsy();
+    scope.$digest();
+    expect(scope.name).toBe('');
+  });
+
+  it('add placeholder on blur empty input', function(){
+    compile();
+    scope.name = '';
+    scope.$digest();
+    element.blur();
+    expect(element.val()).toEqual(element.attr('placeholder'));
+    expect(element.hasClass(className)).toBeTruthy();
+    scope.$digest();
+    expect(scope.name).toBe('');
+  });
+
+
+  afterEach(function(){
+    element.remove();
+    scope.$destroy();
+  });
+});
