b157af617173ab3130b933068f5d6579eebc9a89
zjuwwq <zjuwwq@163.com>
2014-04-25 15:48:43 +0800

    update unit test config

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -8,21 +8,12 @@ module.exports = function(config) {
 		files: [
 			'bower_components/angular/angular.js',
 			'bower_components/angular-mocks/angular-mocks.js',
+			'bower_components/jquery/dist/jquery.js',
 			'src/*.js',
 			'test/*.spec.js'
 		],
-		// list of files to exclude
-		exclude: [
-
-		],
-		// test results reporter to use
-		// possible values: 'dots', 'progress', 'junit'
-		reporters: ['dots'],
-		// level of logging
-		// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-		logLevel: config.LOG_INFO,
 		// enable / disable watching file and executing tests whenever any file changes
-		autoWatch: false,
+		autoWatch: true,
 		// Start these browsers, currently available:
 		// - Chrome
 		// - ChromeCanary
@@ -41,11 +32,6 @@ module.exports = function(config) {
 				base: 'IE',
 				'x-ua-compatible': 'IE=EmulateIE8'
 			}
-		},
-		// If browser does not capture in given timeout [ms], kill it
-		captureTimeout: 60000,
-		// Continuous Integration mode
-		// if true, it capture browsers, run tests and exit
-		singleRun: false
+		}
 	});
 };
